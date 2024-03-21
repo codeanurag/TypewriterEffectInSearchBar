22bf064112066b1fdb2ef42716e5cbc37957c952
Anurag Pandit <codeanuragpandit@gmail.com>
2024-03-21 17:15:00 +0530

    Initial commit to push the project

---
+++ TypewiterEffect SearchBar/TypewiterEffect SearchBar/TypewiterEffect_SearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypewiterEffect_SearchBarApp.swift
+//  TypewiterEffect SearchBar
+//
+//  Created by Anurag Pandit on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TypewiterEffect_SearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
